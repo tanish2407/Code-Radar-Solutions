b34e30c5bf779fb994b3c2d4fec1a947fd8cea3c
tanish2407 <tanish0720.becse24@chitkara.edu.in>
2025-01-30 21:24:07 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -8,6 +8,21 @@ int main() {
         case 'A':
             printf("Excellent");
             break;
+        case 'B':
+            printf("Good");
+            break;
+        case 'C':
+            printf("Average");
+            break;
+        case 'D':
+            printf("Below Average");
+            break;
+        case 'F':
+            printf("Fail");
+            break;
+        default:
+            printf("Invalid grade");
+            
     }
     return 0;
 }
